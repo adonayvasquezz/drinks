c7715ab7e85fff3ab3d4979a7371f5b21b3a9909
Jesus Vasquez <adonay16.vz@gmail.com>
2023-03-28 13:27:49 -0600

    feat: drinks list render

--- src/components/home/DrinkCard/DrinkCard.tsx
+++ src/components/home/DrinkCard/DrinkCard.tsx
@@ -1,13 +1,22 @@
 import { IDrink } from "@/interfaces/IDrink";
+import Image from "next/image";
 import React, { FC } from "react";
+import css from "./DrinkCard.module.css";
 
 interface Props {
   drink: IDrink;
 }
 
 const Drinkcard: FC<Props> = ({ drink }) => {
-  const { idDrink, strDrink, strDrinkThumb } = drink;
-  return <div>{strDrink}</div>;
+  const { strDrink, strDrinkThumb } = drink;
+  return (
+    <div className={css.cardContainer}>
+      <div className={css.title}>{strDrink}</div>
+      <div className={css.imageContainer}>
+        <Image src={strDrinkThumb} fill className={css.image} alt={strDrink} />
+      </div>
+    </div>
+  );
 };
 
 export default Drinkcard;
--- src/components/home/DrinkList/DrinkList.tsx
+++ src/components/home/DrinkList/DrinkList.tsx
@@ -1,12 +1,20 @@
 import { IDrinkProp } from "@/interfaces/IDrink";
 import React, { FC } from "react";
 import Drinkcard from "../DrinkCard/DrinkCard";
+import css from "./DrinkList.module.css";
 
 const DrinkList: FC<IDrinkProp> = ({ drinks }) => {
+  // const drinksRender =
+  //   drinks.length && drinks.length > 20 ? drinks.splice(1, 21) : drinks;
+
   return (
-    <div>
-      {drinks.length &&
-        drinks.map((drink) => <Drinkcard key={drink.idDrink} drink={drink} />)}
+    <div className={css.container}>
+      <div className={css.grid}>
+        {drinks.length &&
+          drinks.map((drink) => (
+            <Drinkcard key={drink.idDrink} drink={drink} />
+          ))}
+      </div>
     </div>
   );
 };
