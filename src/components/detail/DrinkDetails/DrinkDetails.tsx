4aa29629b9cba501496d2984e972ce8c94132b4f
Jesus Vasquez <adonay16.vz@gmail.com>
2023-03-30 17:38:56 -0600

    feat: render ingredients list

--- src/components/detail/DrinkDetails/DrinkDetails.tsx
+++ src/components/detail/DrinkDetails/DrinkDetails.tsx
@@ -1,10 +1,51 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import Image from "next/image";
+import css from "./DrinkDetails.module.css";
 
 interface Props {
   drink: any;
 }
+
 const DrinkDetails: FC<Props> = ({ drink }) => {
-  return <div>{drink.strDrink}</div>;
+  const [ingredients, setIngredients] = useState<any[]>([]);
+
+  useEffect(() => {
+    const objectToArray = Object.entries(drink);
+    const ingredArr = objectToArray.filter((ingred: any) => {
+      const currentIngred = ingred[0].slice(0, -1);
+      const ingredientField = "strIngredient";
+      return currentIngred === ingredientField;
+    });
+    setIngredients(ingredArr);
+  }, [drink]);
+
+  return (
+    <>
+      <div className={css.imageContainer}>
+        <Image
+          src={drink.strDrinkThumb}
+          fill
+          priority
+          className={css.image}
+          alt={drink.strDrink}
+          sizes="(max-width: 768px) 80vw,
+            (max-width: 1200px) 45vw,
+            25vw"
+        />
+      </div>
+      <div>
+        <h1>{drink.strDrink}</h1>
+        <p>{drink.strInstructions}</p>
+        <p>{drink.strGlass}</p>
+
+        <h3>Ingredients:</h3>
+        <ul>
+          {ingredients &&
+            ingredients.map((el: any) => <li key={el[0]}>{el[1]}</li>)}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default DrinkDetails;
