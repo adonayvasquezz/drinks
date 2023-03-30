3b51ac4896e698b1660853316ae72c44e85fa0ad
Jesus Vasquez <adonay16.vz@gmail.com>
2023-03-30 12:49:51 -0600

    feat: navigation to Drink details page

--- src/components/home/DrinkCard/DrinkCard.tsx
+++ src/components/home/DrinkCard/DrinkCard.tsx
@@ -1,5 +1,6 @@
 import { IDrink } from "@/interfaces/IDrink";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React, { FC } from "react";
 import css from "./DrinkCard.module.css";
 
@@ -8,19 +9,29 @@ interface Props {
 }
 
 const Drinkcard: FC<Props> = ({ drink }) => {
-  const { strDrink, strDrinkThumb } = drink;
+  const { strDrink, strDrinkThumb, idDrink } = drink;
+  const router = useRouter();
+  const handleDetail = (idDrink: string) => {
+    router.push(`/${idDrink}`);
+  };
+
   return (
-    <div className={css.cardContainer}>
-      <div className={css.imageContainer}>
-        <Image
-          src={`${strDrinkThumb}/preview`}
-          fill
-          className={css.image}
-          alt={strDrink}
-        />
+    <a onClick={() => handleDetail(idDrink)}>
+      <div className={css.cardContainer}>
+        <div className={css.imageContainer}>
+          <Image
+            src={`${strDrinkThumb}/preview`}
+            fill
+            className={css.image}
+            alt={strDrink}
+            sizes="(max-width: 768px) 80vw,
+            (max-width: 1200px) 45vw,
+            25vw"
+          />
+        </div>
+        <div className={css.title}>{strDrink}</div>
       </div>
-      <div className={css.title}>{strDrink}</div>
-    </div>
+    </a>
   );
 };
 
